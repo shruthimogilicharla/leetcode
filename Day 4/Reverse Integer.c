02aaed3d5cc3d32e754d726784fafaba3aed0acb
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-08-21 20:29:29 +0530

    Day 4 - Added Reverse Integer in C

---
+++ Day 4/Reverse Integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int reverse(int x)
+{
+    long rev=0;
+    while(x!=0)
+    {
+        int rem=x%10;
+        rev=rev*10+rem;
+        x/=10;
+    }
+    if(rev>2147483647||rev<-2147483648)
+    return 0;
+    return (int)rev;
+}
