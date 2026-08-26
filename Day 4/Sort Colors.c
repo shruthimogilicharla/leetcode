ff6443e53771b2c31277be5dc064e985866bfeae
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-08-26 23:16:44 +0530

    Day 4 - Added Sort Colors in C

---
+++ Day 4/Sort Colors.c
@@ -0,0 +1,19 @@
+void sortColors(int* nums, int n) {
+    int i = 0, j = 0, k = n - 1, temp;
+
+    while (j <= k) {
+        if (nums[j] == 0) {
+            temp = nums[i];
+            nums[i++] = nums[j];
+            nums[j++] = temp;
+        }
+        else if (nums[j] == 2) {
+            temp = nums[j];
+            nums[j] = nums[k];
+            nums[k--] = temp;
+        }
+        else {
+            j++;
+        }
+    }
+}
