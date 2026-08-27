31c41db1872adb8c362ec3c6d3461999b1d98186
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-08-27 16:38:27 +0530

    Day 5 - Added Flipping an Image in C
    The function flips each row of the image and inverts the pixel values from 0 to 1 and vice versa.

---
+++ Day 5/Flipping an Image.c
@@ -0,0 +1,39 @@
+int** flipAndInvertImage(int** image, int imageSize, int* imageColSize,
+                         int* returnSize, int** returnColumnSizes)
+{
+    int i, j, temp;
+
+    // Reverse each row
+    for (i = 0; i < imageSize; i++)
+    {
+        for (j = 0; j < imageColSize[i] / 2; j++)
+        {
+            temp = image[i][j];
+
+            image[i][j] = image[i][imageColSize[i] - 1 - j];
+
+            image[i][imageColSize[i] - 1 - j] = temp;
+        }
+    }
+
+    // Invert 0 and 1
+    for (i = 0; i < imageSize; i++)
+    {
+        for (j = 0; j < imageColSize[i]; j++)
+        {
+            if (image[i][j] == 0)
+            {
+                image[i][j] = 1;
+            }
+            else
+            {
+                image[i][j] = 0;
+            }
+        }
+    }
+
+    *returnSize = imageSize;
+    *returnColumnSizes = imageColSize;
+
+    return image;
+}
