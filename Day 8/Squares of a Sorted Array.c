8b132064f7ae765ac3d710392f4a9ac4dcc172e9
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-08-31 17:42:12 +0530

    Day 8 - Added Squares of a Sorted Array in C

--- Day 8/Squares of a Sorted Array.c
+++ Day 8/Squares of a Sorted Array.c
@@ -1,7 +1,7 @@
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
     int i, j, temp;
     // Square every element
-    
+
     for (i = 0; i < numsSize; i++) {
         nums[i] = nums[i] * nums[i];
     }
