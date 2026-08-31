2d47f4c5923424e96263729476f2c1744c716f4e
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-08-31 17:40:11 +0530

    Day 8 - Added Squares of a Sorted Array in C

---
+++ Day 8/Squares of a Sorted Array.c
@@ -0,0 +1,22 @@
+int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+    int i, j, temp;
+
+    // Square every element
+    for (i = 0; i < numsSize; i++) {
+        nums[i] = nums[i] * nums[i];
+    }
+
+    // Sort the squared values
+    for (i = 0; i < numsSize - 1; i++) {
+        for (j = i + 1; j < numsSize; j++) {
+            if (nums[i] > nums[j]) {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+
+    *returnSize = numsSize;
+    return nums;
+}
