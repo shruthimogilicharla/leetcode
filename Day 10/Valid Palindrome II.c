40d10881c62cb3f93e1cd494ef288352780bb1f1
shruthimogilicharla <shruthimogilicharla02@gmail.com>
2026-09-02 18:08:38 +0530

    Day 10 - Added Valid Palindrome II in C

---
+++ Day 10/Valid Palindrome II.c
@@ -0,0 +1,40 @@
+#include <stdbool.h>
+
+bool isPalindrome(char *s, int left, int right)
+{
+    while (left < right)
+    {
+        if (s[left] != s[right])
+            return false;
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+bool validPalindrome(char *s)
+{
+    int left = 0;
+    int right = 0;
+
+    while (s[right] != '\0')
+        right++;
+
+    right--;
+
+    while (left < right)
+    {
+        if (s[left] != s[right])
+        {
+            return isPalindrome(s, left + 1, right) ||
+                   isPalindrome(s, left, right - 1);
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
